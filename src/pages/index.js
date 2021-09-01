b0da35da9cd47670b8486d683e3cb6e1729c700d
Jason Johns <jason@jasonjohns.com>
2021-09-01 15:35:29 -0400

    fixed file paths for index image

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
             <p>I'm making this by following the Gatsby Tutorial.</p>
             <StaticImage
                 alt="Clifford, a reddish-brown pitbull, posing on the couch"
-                src="../src/images/daily-morning-awesomeness-40-photos-108-33-12.jpg"
+                src="../images/daily-morning-awesomeness-40-photos-108-33-12.jpg"
             />
         </Layout>
     )
