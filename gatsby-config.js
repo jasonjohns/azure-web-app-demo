c83b1e28335391a644d2a71d03d6bc9a0a17fe08
Jason Johns <jason@jasonjohns.com>
2021-09-01 16:26:06 -0400

    completed gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://azure-web-app-demo.jasonjohns.com",
-    title: "Azure Web App Demo",
+    title: "Gataby on Azure!",
   },
   plugins: [  "gatsby-plugin-theme-ui",
               "gatsby-plugin-image",
               "gatsby-plugin-sharp",
+              {
+                resolve: "gatsby-source-filesystem",
+                options: {
+                  name: `blog`,
+                  path: `${__dirname}/blog`,
+                }
+              },
            ],
 };
